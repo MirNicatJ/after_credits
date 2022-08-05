5c8b0c68a098f09a0f0b62ae17fed0e30c679665
mirnijatj <mirnijatj@crocusoft.com>
2022-08-05 12:46:26 +0400

    New modules added

---
+++ domain/src/main/java/com/mirnijat/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package com.mirnijat.domain
+
+class MyClass {
+}
--- presentation/src/main/java/com/mirnijat/presentation/MainActivity.kt
+++ presentation/src/main/java/com/mirnijat/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.mirnijat.aftercredits
+package com.mirnijat.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
